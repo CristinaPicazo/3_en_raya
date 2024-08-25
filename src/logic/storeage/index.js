015253843ff83e71e755b660fd2aa5a9fd1b0890
CristinaPicazo <cristina.picazo.merlos@gmail.com>
2024-08-25 22:25:04 +0200

    Updates

--- src/logic/storeage/index.js
+++
@@ -1,9 +0,0 @@
-export const guardarJuego = ({ tablero, turno }) => {
-  window.localStorage.setItem("tablero", JSON.stringify(tablero));
-  window.localStorage.setItem("turno", turno);
-};
-
-export const resetJuegoStorage = () => {
-  window.localStorage.removeItem("tablero");
-  window.localStorage.removeItem("turno");
-};
